9ddb362903d32b9547ffa1cf45b98d46428a4b57
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-06-10 12:10:27 +0900

    questionのfetchを実装

---
+++ src/frontend/server/api/question.ts
@@ -0,0 +1,11 @@
+import { defineEventHandler, getQuery } from "h3";
+import type { Questions } from "~/composables/questionInterface";
+
+export default defineEventHandler(async (event) => {
+    const uri = getQuery(event).uri as string;
+    const BASE_API = useRuntimeConfig().public.BASE_API;
+
+    const data = await $fetch<Questions>(`${BASE_API}${uri}`);
+
+    return data;
+});
