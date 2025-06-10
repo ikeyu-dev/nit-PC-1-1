aece43689cef7dcf3808b1f31fa35a99fc95cfe7
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-06-10 17:36:39 +0900

    余分な改行を削除

--- src/frontend/server/api/question.ts
+++ src/frontend/server/api/question.ts
@@ -4,8 +4,6 @@ import type { Questions } from "~/composables/questionInterface";
 export default defineEventHandler(async (event) => {
     const uri = getQuery(event).uri as string;
     const BASE_API = useRuntimeConfig().public.BASE_API;
-
     const data = await $fetch<Questions>(`${BASE_API}${uri}`);
-
     return data;
 });
