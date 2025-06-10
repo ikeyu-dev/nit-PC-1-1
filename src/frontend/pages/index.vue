aa1efbe0886b572a3851af8931614a0d0b82b280
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-06-10 12:17:49 +0900

    UIの変更

--- src/frontend/pages/index.vue
+++ src/frontend/pages/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import FaceMeshCanvas from "~/components/camera.vue";
-import Question from "~/components/question.vue";
+import FaceMeshCanvas from "~/components/Camera.vue";
+import PcShow from "~/components/Question.vue";
 </script>
 
 <template>
-    <div>
-        <FaceMeshCanvas />
-        <Question />
+    <div
+        class="top-0 grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center"
+    >
+        <div class="flex justify-center items-center">
+            <FaceMeshCanvas />
+        </div>
+
+        <div class="flex justify-center items-center">
+            <PcShow />
+        </div>
     </div>
 </template>
