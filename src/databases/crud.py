291fa1e657859c37c8453a369a24b0a572e09077
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-07-16 10:54:56 +0900

    importに関する修正

--- src/databases/crud.py
+++ src/databases/crud.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from sqlalchemy.orm import Session
 
-import src.databases.schema as themes_schema
-import src.databases.model as questions_model
+import src.databases.schema as schema
+import src.databases.model as model
 
 
-def create_question(db: Session, question: themes_schema.Question):
+def create_question(db: Session, question: schema.Question):
     try:
-        new_question = questions_model.Question(
+        new_question = model.Question(
             question=question.question,
             tag=question.tag,
             detail=question.detail,
@@ -23,20 +23,33 @@ def create_question(db: Session, question: themes_schema.Question):
 
 
 def get_all_question(db: Session):
-    question = db.query(questions_model.Question).all()
+    question = db.query(model.Question).all()
     if not question:
         return False
     return question
 
 
 def delete_question(db: Session, question_id: int):
-    question = (
-        db.query(questions_model.Question)
-        .filter(questions_model.Question.id == question_id)
-        .first()
-    )
+    question = db.query(model.Question).filter(model.Question.id == question_id).first()
     if not question:
         return False
     db.delete(question)
     db.commit()
     return True
+
+
+def create_score(db: Session, score: schema.Score):
+    try:
+        new_score = model.Score(
+            id=score.id,
+            score=score.score,
+            nickname=score.nickname,
+            created_at=datetime.now(),
+        )
+        db.add(new_score)
+        db.commit()
+        db.refresh(new_score)
+    except Exception as e:
+        db.rollback()
+        return False
+    return score
--- src/databases/schema.py
+++ src/databases/schema.py
@@ -8,9 +8,6 @@ class Question(BaseModel):
     detail: str
 
 
-class ThemeResponse(Question):  # レスポンス用モデル
-    id: int
-    created_at: datetime.datetime  # DBから取得した値を含む
-
-    # Pydantic v2 の場合
-    model_config = ConfigDict(from_attributes=True)
+class Score(BaseModel):
+    score: int
+    nickname: str
